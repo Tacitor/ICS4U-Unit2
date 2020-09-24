d8b858728de90db712a7be8ede59ea04bf28a0c9
lukra1175 <lukra1175@8T71C62.Student.UGDSB.ED>
2020-09-24 15:01:53 -0400

    Add Introducing and Defining Classes Part 1 Project 2

---
+++ Objects and Classes/Introducing and Defining Classes Part 1/IntroducingAndDefiningClasses1_2/src/introducinganddefiningclasses1_2/Dvd.java
@@ -0,0 +1,77 @@
+/*
+ * Lukas Krampiz
+ * Sep 24, 2020
+ * 
+ */
+package introducinganddefiningclasses1_2;
+
+/**
+ *
+ * @author lukra1175
+ */
+public class Dvd {
+    private String title;
+    private String genre;
+    private int year;
+    private boolean loan;
+    private String loanee;
+    
+    public Dvd(String name, String genre, int year) {
+        title = name;
+        this.genre = genre;
+        this.year = year;
+        loan = false;
+        loanee = "No one!";
+    }
+    public Dvd(String name, String genre, int year, boolean loan, String loanee) {
+        title = name;
+        this.genre = genre;
+        this.year = year;
+        this.loan = loan;
+        this.loanee = loanee;
+    }    
+    
+    public void setTitle(String name) {
+        title = name;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void signOut(String loanee) {
+        loan = true;
+        this.loanee = loanee;
+    }
+    public void signIn() {
+        loan = false;
+        loanee = "No one!";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public int getYear() {
+        return year;
+    }
+    public boolean isLoaned() {
+        return loan;
+    }
+    public String getLoanee() {
+        return loanee;
+    }
+
+    public String toString() {
+        return "Title: " + title + 
+                "Genre: " + genre + 
+                "Year produced: " + year + 
+                "On loan? " + loan + 
+                "Loaned to: " + loanee;
+    }
+
+}
---
+++ Objects and Classes/Introducing and Defining Classes Part 1/IntroducingAndDefiningClasses1_2/src/introducinganddefiningclasses1_2/IntroducingAndDefiningClasses1_2.java
@@ -0,0 +1,21 @@
+/*
+ * Lukas Krampiz
+ * Sep 24, 2020
+ * 
+ */
+package introducinganddefiningclasses1_2;
+
+/**
+ *
+ * @author lukra1175
+ */
+public class IntroducingAndDefiningClasses1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
