0f5d281b13c29463b0887bfc5d50167c996c63a1
Lukas <Lukas@DESKTOP-TQJA52Q>
2020-10-05 19:05:44 -0400

    Update Classes Continued Part 2 Project 2

---
+++ Classes Continued/Classes Continued - Part 2/EmployeePayrollSystem/src/employeepayrollsystem/PartTimeEmployee.java
@@ -0,0 +1,23 @@
+/*
+ * B Cutten
+ * Sept 2014
+ * Represents...A Part time employee that never have overtime. Also extends the abstract Employee class.
+ */
+package employeepayrollsystem;
+
+public class PartTimeEmployee extends Employee{
+    
+    //Constructor!
+    //purpose is to call the constructor from the super class
+    public PartTimeEmployee(){
+        super();
+    }
+    
+    //Purpose is to calculate how much money an employee earned
+    public double getPay(){
+        double pay;        
+        pay = rate * hours;
+        totalPay = totalPay + pay;
+        return pay;
+    }
+}
