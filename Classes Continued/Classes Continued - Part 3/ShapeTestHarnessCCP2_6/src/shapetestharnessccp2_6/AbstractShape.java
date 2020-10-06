733cf0d052a0917eed5ac630b7df3f5c824970a4
lukra1175 <lukra1175@8T71C62.Student.UGDSB.ED>
2020-10-06 14:31:07 -0400

    Add Classes Continued Part 3 Project 6

---
+++ Classes Continued/Classes Continued - Part 3/ShapeTestHarnessCCP2_6/src/shapetestharnessccp2_6/AbstractShape.java
@@ -0,0 +1,53 @@
+/*
+ * Brad Cutten
+ * Sept 2014
+ * An abstract class which ...
+ */
+package shapetestharnessccp2_6;
+import TurtleGraphics.Pen;
+
+abstract public class AbstractShape implements Shape {
+    
+    protected double xPos;
+    protected double yPos;
+    
+  
+    public AbstractShape (){
+        xPos = 0;
+        yPos = 0;        
+    }
+    
+    public AbstractShape(double xLoc, double yLoc){
+        this();
+        xPos = xLoc;
+        yPos = yLoc;
+    }
+    
+  
+    abstract public double area();
+    
+    abstract public void draw(Pen p);
+  
+    public final double getXPos(){
+        return xPos;
+    }
+    
+    public final double getYPos(){
+        return yPos;
+    }
+    
+    abstract public double getPerim();
+   
+    public void move(double xLoc, double yLoc){
+        xPos = xLoc;
+        yPos= yLoc;
+    }
+    
+    abstract public void stretchBy(double factor);
+  
+    public String toString(){
+        String str= "(X,Y) Position: (" + xPos + "," + yPos + ")\n" 
+                + "Area: " + area();
+        return str;
+    }
+}
