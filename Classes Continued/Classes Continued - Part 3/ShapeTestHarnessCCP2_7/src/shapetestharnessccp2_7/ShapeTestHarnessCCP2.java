7e315fe3512064ca40a4b213a8474af6ed755ae0
Lukas <Lukas@DESKTOP-TQJA52Q>
2020-10-06 17:33:00 -0400

    Finish Classes Continued Part 3 Project 7

--- Classes Continued/Classes Continued - Part 3/ShapeTestHarnessCCP2_7/src/shapetestharnessccp2_7/ShapeTestHarnessCCP2.java
+++ Classes Continued/Classes Continued - Part 3/ShapeTestHarnessCCP2_7/src/shapetestharnessccp2_7/ShapeTestHarnessCCP2.java
@@ -16,20 +16,32 @@ public class ShapeTestHarnessCCP2 {
         Shape s1 = new Circle(20,20,20);
         Shape s2 = new Rect(-50,-50,10,20);
         Shape s3 = new Wheel(-20,-20,20,6);
+        Shape s4 = new Triangle(0,0,30,-30,-30,-30);
         
         Shape s2VerTwo = new Rect(-50,-50,10,20);
         
         Shape s1VerTwo = ((Circle)s1).clone();
         
+        Shape Tri2 = ((Triangle)s4).clone();
+        
+        //move the triangle off the other shapes
+        s4.move(50, -10);
+        
+        s4.stretchBy(2);
+        
+        s4.move(0, 120);
+        
         s1.draw(p);
         s2.draw(p);
         s3.draw(p);
+        s4.draw(p);
         //p.setColor(Color.red);
         //s1VerTwo.draw(p);
         
         System.out.println(s1); 
         System.out.println(s2); 
         System.out.println(s3);
+        System.out.println(s4);
         
         //System.out.println(((Rect)s2).equals((Rect)s2VerTwo));
         
@@ -38,6 +50,8 @@ public class ShapeTestHarnessCCP2 {
         //System.out.println(s2.getPerim());
         //System.out.println(s3.getPerim());
         //System.out.println(s1.getPerim());
+        
+        //System.out.println(((Triangle)s4).equals((Triangle)Tri2));
     }
     
 }
--- Classes Continued/Classes Continued - Part 3/ShapeTestHarnessCCP2_7/src/shapetestharnessccp2_7/Triangle.java
+++ Classes Continued/Classes Continued - Part 3/ShapeTestHarnessCCP2_7/src/shapetestharnessccp2_7/Triangle.java
@@ -11,13 +11,13 @@ import TurtleGraphics.Pen;
  *
  * @author lukra1175
  */
-public class Triangle extends AbstractShape{
-    
+public class Triangle extends AbstractShape {
+
     protected double xPos2;
     protected double yPos2;
     protected double xPos3;
     protected double yPos3;
-    
+
     public Triangle() {
         super();
         xPos2 = 0;
@@ -25,7 +25,7 @@ public class Triangle extends AbstractShape{
         xPos3 = 0;
         yPos3 = 0;
     }
-    
+
     public Triangle(double xLoc, double yLoc, double xLoc2, double yLoc2, double xLoc3, double yLoc3) {
         super(xLoc, yLoc);
         xPos2 = xLoc2;
@@ -33,40 +33,72 @@ public class Triangle extends AbstractShape{
         xPos3 = xLoc3;
         yPos3 = yLoc3;
     }
-    
+
     public double area() {
-        
+        return (Math.abs(xPos * yPos2 - xPos2 * yPos + xPos2 * yPos3 - xPos3 * yPos2 + xPos3 * yPos - xPos * yPos3) / 2);
     }
-    
+
     public void draw(Pen p) {
-        
+        p.up();
+        p.move(xPos, yPos);
+        p.down();
+        p.move(xPos2, yPos2);
+        p.move(xPos3, yPos3);
+        p.move(xPos, yPos);
     }
-    
+
     public double getPerim() {
-        
+        double sum = 0;
+
+        sum += Math.sqrt((xPos - xPos2) * (xPos - xPos2) + (yPos - yPos2) * (yPos - yPos2));
+        sum += Math.sqrt((xPos - xPos3) * (xPos - xPos3) + (yPos - yPos3) * (yPos - yPos3));
+        sum += Math.sqrt((xPos3 - xPos2) * (xPos3 - xPos2) + (yPos3 - yPos2) * (yPos3 - yPos2));
+
+        return sum;
     }
     
-    public void move(double xLoc, double yLoc) {
-        
+    /**
+     * Mutate the position of the triangle
+     * @param xTran The translation in the x
+     * @param yTran The translation in the y
+     */
+    public void move(double xTran, double yTran) {
+        xPos += xTran;
+        xPos2 += xTran;
+        xPos3 += xTran;
+        yPos += yTran;
+        yPos2 += yTran;
+        yPos3 += yTran;
     }
-    
+
     public void stretchBy(double factor) {
-        
+        xPos2 = xPos + (xPos2 - xPos) * factor;
+        xPos3 = xPos + (xPos3 - xPos) * factor;
+        yPos2 = yPos + (yPos2 - yPos) * factor;
+        yPos3 = yPos + (yPos3 - yPos) * factor;
     }
-    
+
     public String toString() {
         String str = "TRIANGLE\n"
-                + "(X,Y) Position: (" + xPos + "," + yPos + ")\n"; //add the other points
-        
+                + "(X1,Y1) Position: (" + xPos + "," + yPos + ")\n"
+                + "(X2,Y2) Position: (" + xPos2 + "," + yPos2 + ")\n"
+                + "(X3,Y3) Position: (" + xPos3 + "," + yPos3 + ")\n"
+                + "Area: " + area();
+
         return str;
     }
-    
+
     public Triangle clone() {
         Triangle copy = new Triangle(xPos, yPos, xPos2, yPos2, xPos3, yPos3);
         return copy;
     }
-    
-    public boolean equals() {
-        return false;
+
+    public boolean equals(Triangle other) {
+        return (other.xPos == this.xPos
+                && other.xPos2 == this.xPos2
+                && other.xPos3 == this.xPos3
+                && other.yPos == this.yPos
+                && other.yPos2 == this.yPos2
+                && other.yPos3 == this.yPos3);
     }
 }
